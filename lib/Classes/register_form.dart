33df4748dc0b10618a37798d302a3060869a0f72
nadeemabualarayes <66251426+nadeemabualarayes@users.noreply.github.com>
2021-06-08 19:47:01 +0300

    8-6-2021

--- lib/Classes/register_form.dart
+++ lib/Classes/register_form.dart
@@ -154,6 +154,8 @@ class _RegisterFormState extends State<RegisterForm> {
                               .doc(user.uid)
                               .set({
                             'email': user.email,
+                            'GoogleEmail':"",
+                            'FacebookEmail':"",
                             'providerId': "password",
                             'uid': user.uid,
                             'password':_passwordController.text
