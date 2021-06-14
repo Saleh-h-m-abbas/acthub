b66a0d7801557b6b9b77bf99f0785fe2c890a85e
halaziq <83180662+halaziq@users.noreply.github.com>
2021-06-14 17:31:23 +0300

    ManagmenetPage
    done

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute:SignUpFormPage.id,
+      initialRoute:ManagementPage.id,
       routes: {
         LandingPage.id: (context) => LandingPage(),
         EnableLocation.id: (context) => EnableLocation(),
